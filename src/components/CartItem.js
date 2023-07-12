4cfe66764860a61b53d0c7adb7638c48e758911d
Arman-Kabir <armankabir8@gmail.com>
2023-07-12 19:16:06 +0600

    cart item done

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -1,7 +1,22 @@
+import { ChevronDown,ChevronUp } from '../icons';
 import React from 'react'
 
-export const CartItem = () => {
-  return (
-    <div>CartItem</div>
-  )
+export const CartItem = ({id,img,title,price,amount}) => {
+  return <article className='cart-item'>
+    <img src={img} alt={title} />
+    <div>
+      <h4>{title}</h4>
+      <h4 className="item-price">${price}</h4>
+      <button className='remove-btn'>remove</button>
+    </div>
+    <div>
+      <button className='amount-btn'>
+        <ChevronUp></ChevronUp>
+      </button>
+      <p className='amount'>{amount}</p>
+      <button className='amount-btn'>
+        <ChevronDown></ChevronDown>
+      </button>
+    </div>
+  </article>
 }
