85bd08c1ecc86dd058b8f51c2704276245cfe91d
Arman-Kabir <armankabir8@gmail.com>
2023-07-12 12:15:28 +0600

    initial state value shown in navabr using useSelector

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,24 @@
 import { CartIcon } from '../icons';
-import { UseSelector } from 'react-redux';
+import { UseSelector, useSelector } from 'react-redux';
 
 
 import React from 'react'
 
 export const Navbar = () => {
+    // console.log(useSelector((store) => { console.log(store) }));
+
+    const {amount}  = useSelector((store)=>store.cart);
+
     return (
         <nav>
             <div className='nav-center'>
                 <h3>redux toolkit</h3>
+                <div className="nav-container">
+                    <CartIcon></CartIcon>
+                    <div className="amount-container">
+                        <p className='total-amount'>{amount}</p>
+                    </div>
+                </div>
             </div>
         </nav>
     )
