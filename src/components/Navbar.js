94bddef480b2c33e971692f08622b2458e9db119
Arman-Kabir <armankabir8@gmail.com>
2023-07-12 11:39:03 +0600

    navbaar component created

---
+++ src/components/Navbar.js
@@ -0,0 +1,16 @@
+import { CartIcon } from '../icons';
+import { UseSelector } from 'react-redux';
+
+
+import React from 'react'
+
+export const Navbar = () => {
+    return (
+        <nav>
+            <div className='nav-center'>
+                <h3>redux toolkit</h3>
+            </div>
+        </nav>
+    )
+}
+
